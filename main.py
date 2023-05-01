e8dc49213b6d9c6fd38321cd64bbf4e2c0906665
Mauryanx <115839207+Mauryanx@users.noreply.github.com>
2023-05-01 16:34:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -14,16 +14,48 @@ def MED(S, T):
         if (S[0] == T[0]):
             return(MED(S[1:], T[1:]))
         else:
-            return(1 + min(MED(S, T[1:]), MED(S[1:], T)))
+            return(1 + min(MED(S, T[1:]), MED(S[1:], T), MED(S[1:], T[1:])))
 
 
 def fast_MED(S, T, MED={}):
     # TODO -  implement memoization
-    pass
+    if (S, T) in MED:
+        return MED[(S, T)]
+    if (S == ""):
+        value = len(T)
+    elif (T == ""):
+        value = len(S)
+    elif (S[0] == T[0]):
+        value = fast_MED(S[1:], T[1:], MED)
+    else:
+        value = 1 + min(fast_MED(S, T[1:], MED), 
+                      fast_MED(S[1:], T, MED),  
+                      fast_MED(S[1:], T[1:], MED))
+    MED[(S, T)] = value
+    return value
+
 
 def fast_align_MED(S, T, MED={}):
     # TODO - keep track of alignment
-    pass
+    if (T == ""):
+        value = (S, '-' * len(S))
+    elif (S == ""):
+        value = ('-' * len(T), T)
+    elif (S[0] == T[0]):
+        aS, aT = fast_align_MED(S[1:], T[1:], MED)
+        value = (S[0] + aS, T[0] + aT)
+    else:
+        minimum_val = min((fast_MED(S[1:], T[1:], MED)), (fast_MED(S, T[1:], MED)), (fast_MED(S[1:], T, MED)))
+        if minimum_val == (fast_MED(S[1:], T[1:], MED)):
+            aS, aT = fast_align_MED(S[1:], T[1:], MED)
+            value = (S[0] + aS, T[0] + aT)
+        elif minimum_val == (fast_MED(S, T[1:], MED)):
+            aS, aT = fast_align_MED(S, T[1:], MED)
+            value = ('-' + aS, T[0] + aT)
+        else:
+            aS, aT = fast_align_MED(S[1:], T, MED)
+            value = (S[0] + aS, '-' + aT)
+    return value
 
 def test_MED():
     for S, T in test_cases:
